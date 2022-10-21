6d2847efef7a7fe538f9c5b0251fff7bf4095141
Angela Dai <zhjh.angela@gmail.com>
2022-10-21 16:13:34 -0400

    intros fully integrated

---
+++ Assets/Scripts/Timeline/StartTimeline.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartTimeline : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Reference to page controller
+    public Menu.PageController pageController;
+    // Reference to PlaceObjectInFrontOfCameraScript
+    public PlaceObjectInFrontOfCamera placeObjectInFrontOfCamera;
+    void Awake()
+    {
+        print("hellooo");
+        placeObjectInFrontOfCamera.PlaceInFrontOfCamera();
+        pageController.TurnPageOff(Menu.PageType.Hotspot, Menu.PageType.Timeline);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
