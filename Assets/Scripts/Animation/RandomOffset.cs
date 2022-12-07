67a001f91db98e175c15af92ff747e69d0d2a3e6
Angela Dai <zhjh.angela@gmail.com>
2022-12-06 23:05:36 -0500

    add more people to crowd

---
+++ Assets/Scripts/Animation/RandomOffset.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomOffset : MonoBehaviour
+{
+    Animator anim;
+    float offset;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetFloat("Offset", Random.Range(0.0f, 1.0f));
+    }
+    
+}
