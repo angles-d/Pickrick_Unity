0931989d0f5b26c379a17b6ba2ef619d5e3875e1
Angela Dai <zhjh.angela@gmail.com>
2023-05-26 16:01:08 -0400

    ar camera working

--- Assets/Scenes/Utils/DontDestroy.cs
+++ Assets/Scenes/Utils/DontDestroy.cs
@@ -10,14 +10,15 @@ public class DontDestroy : MonoBehaviour
     private void Awake()
     {
         //Tag objects w. don't destroy in inspector
-        GameObject[] objs = GameObject.FindGameObjectsWithTag("DontDestroy");
+        //GameObject[] objs = GameObject.FindGameObjectsWithTag("DontDestroy");
 
         //2 GameObjects: AR Session Origin, AR Session (if adding more 
-        if (objs.Length > numDontDestroy)
+        if (gameObject.scene.buildIndex != 0)
         {
+            Debug.Log("Destroyed" + gameObject.name);
             Destroy(this.gameObject);
         }
-        DontDestroyOnLoad(this.gameObject);
 
+        DontDestroyOnLoad(this.gameObject);
     }
 }
