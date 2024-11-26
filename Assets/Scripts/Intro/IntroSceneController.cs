3e1724cfd3f336d00a1ed71ada103ed61da19ebf
Sanjana Date <68459515+sanjanadate@users.noreply.github.com>
2024-11-25 20:06:42 -0500

    fall 24 intro

---
+++ Assets/Scripts/Intro/IntroSceneController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class IntroSceneController : MonoBehaviour
+{
+    public void BeginExperience()
+    {
+        Debug.Log("starting AR experience");
+        SceneManager.LoadScene(1);
+    }
+}
