762da51fb9a14ee57b99355b605a4c94d9a47463
Sanjana Date <68459515+sanjanadate@users.noreply.github.com>
2024-11-25 20:05:08 -0500

    f24 restart

---
+++ Assets/Scripts/Utils/RestartSceneController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RestartSceneController : MonoBehaviour
+{
+    public GameObject confirm;
+    public Button restartIconButton;
+
+    public void OnRestartButtonClick()
+    {
+        Time.timeScale = 0f;
+        restartIconButton.interactable = false;
+        confirm.SetActive(true);
+    }
+
+    public void RestartApplication()
+    {
+        //Reset AR references
+        if (GameObject.Find("AR Session") != null) Destroy(GameObject.Find("AR Session"));
+        if (GameObject.Find("AR Session Origin") != null) Destroy(GameObject.Find("AR Session Origin"));
+        if (GameObject.Find("LocationInfo") != null) Destroy(GameObject.Find("LocationInfo"));
+        SceneManager.LoadScene(0);
+    }
+
+    public void ResumeApplication()
+    {
+        Time.timeScale = 1f;
+        restartIconButton.interactable = true;
+        confirm.SetActive(false);
+    }
+
+    public void StartARExperience()
+    {
+        Debug.Log("starting AR experience");
+        SceneManager.LoadScene(1);
+    }
+}
